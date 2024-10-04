4ebe032785bd2a0c2b92e1426850469205f69564
PauloIran1013 <julioirancoelho@gmail.com>
2024-10-04 14:27:09 -0300

    criei a pasta

---
+++ teste.sql
@@ -0,0 +1,27 @@
+drop database aaa;
+
+create database aaa;
+
+use aaa;
+
+create table t1(
+id_t1 int auto_increment primary key, 
+);
+
+create table t2(
+id_t2 int auto_increment primary key,
+);
+
+
+
+insert into t1()values
+
+
+insert into t2()values
+
+
+
+
+--vizu--
+show tables
+select * from 
